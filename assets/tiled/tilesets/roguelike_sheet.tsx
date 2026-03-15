f7efa1bbcdfcbc60b9294413bb7700fa922f43d4
Paul Sheridan <paul.sheridan@me.com>
2026-03-14 19:08:21 -0700

    update overworld tilemap

--- assets/tiled/tilesets/roguelike_sheet.tsx
+++ assets/tiled/tilesets/roguelike_sheet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="roguelike_sheet" tilewidth="16" tileheight="16" spacing="1" margin="0" tilecount="1767" columns="57">
- <image source="../../roguelikeSheet_transparent.png" width="968" height="526"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tiny_town_overworld" tilewidth="16" tileheight="16" spacing="1" margin="0" tilecount="132" columns="12">
+ <image source="../../Tiny Town/Tilemap/tilemap.png" width="203" height="186"/>
 </tileset>
